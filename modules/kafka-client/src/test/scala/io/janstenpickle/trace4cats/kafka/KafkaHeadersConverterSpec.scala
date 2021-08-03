a2b9091f5ceec18ad11bce8f3b53a1c9c05324cd
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-08-03 07:21:45 +0300

    Add Scala 3 build

--- modules/kafka-client/src/test/scala/io/janstenpickle/trace4cats/kafka/KafkaHeadersConverterSpec.scala
+++ modules/kafka-client/src/test/scala/io/janstenpickle/trace4cats/kafka/KafkaHeadersConverterSpec.scala
@@ -11,7 +11,7 @@ import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 class KafkaHeadersConverterSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks with ArbitraryInstances {
   behavior.of("KafkaHeaders.converter")
 
-  it should "convert headers isomorphically" in forAll { traceHeaders: TraceHeaders =>
+  it should "convert headers isomorphically" in forAll { (traceHeaders: TraceHeaders) =>
     assert(Eq.eqv(traceHeaders, converter.from(converter.to(traceHeaders))))
   }
 
