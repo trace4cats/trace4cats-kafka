417c2cb943b82062aae7ea3d22b2beea7cf4567d
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-08-03 07:21:45 +0300

    Add Scala 3 build

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,9 @@ object Dependencies {
   object Versions {
     val scala212 = "2.12.14"
     val scala213 = "2.13.6"
+    val scala3 = "3.0.1"
 
-    val trace4cats = "0.12.0-RC2"
+    val trace4cats = "0.12.0-RC2+18-2de6b68e"
 
     val fs2Kafka = "2.1.0"
 
