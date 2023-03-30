306df943afa6c58410cc2de6174084886195f237
Ondra Pelech <ondra.pelech@gmail.com>
2023-03-30 15:56:28 +0200

    Add test

--- modules/kafka-client/src/test/scala/trace4cats/kafka/KafkaHeadersConverterSpec.scala
+++ modules/kafka-client/src/test/scala/trace4cats/kafka/KafkaHeadersConverterSpec.scala
@@ -1,7 +1,7 @@
 package trace4cats.kafka
 
 import cats.Eq
-import fs2.kafka.{Header, Headers}
+import fs2.kafka.{Header, Headers, ProducerRecord, ProducerRecords}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 import trace4cats.kafka.KafkaHeaders.converter
@@ -20,5 +20,23 @@ class KafkaHeadersConverterSpec extends AnyFlatSpec with ScalaCheckDrivenPropert
     val expected = TraceHeaders.of("header1" -> "value1", "header2" -> "value2")
 
     assert(Eq.eqv(converter.from(headers), expected))
+  }
+
+  it should "append tracing headers" in {
+    val headers = Headers(Header("header1", "value1"), Header("header2", "value2"))
+    val traceHeaders = TraceHeaders.of("header2" -> "value2new", "header3" -> "value3")
+
+    val prOrig = ProducerRecords(List(ProducerRecord("topic", "key", "vale").withHeaders(headers)))
+    val pr = TracedProducer.addHeaders(traceHeaders)(prOrig)
+
+    val headersActual = pr.records.toList.flatMap(_.headers.toChain.toList)
+    val headersExpected = List(
+      Header("header1", "value1"),
+      Header("header2", "value2"),
+      Header("header2", "value2new"),
+      Header("header3", "value3")
+    )
+
+    assert(Eq.eqv(headersExpected, headersActual))
   }
 }
